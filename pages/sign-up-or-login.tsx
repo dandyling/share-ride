9dc7db270a32e906dc6d46ae0074ed73dfd09447
Dandy Kwong <dandyling@gmail.com>
2022-08-06 22:54:32 +0800

    Style login page

--- pages/sign-up-or-login.tsx
+++ pages/sign-up-or-login.tsx
@@ -1,30 +1,42 @@
-import { Button } from "@mantine/core";
+import { Button, Title, Text } from "@mantine/core";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { FaArrowRight } from "react-icons/fa";
 import { Layout } from "../components/layout";
+import { AiFillCar } from "react-icons/ai";
 
 const SignUpOrLogin: NextPage = () => {
   const router = useRouter();
   return (
     <Layout>
-      <div className="flex flex-col justify-center h-full p-8 space-y-4">
-        <Button
-          size="lg"
-          className="bg-primary"
-          onClick={() => router.push("/signout")}
-        >
-          Sign Up
-        </Button>
-        <Button
-          size="lg"
-          variant="outline"
-          className="text-primary border-primary"
-          rightIcon={<FaArrowRight className="text-sm" />}
-          onClick={() => router.push("/login")}
-        >
-          Login
-        </Button>
+      <div className="flex flex-col items-center justify-center h-full p-16 space-y-16">
+        <div className="flex flex-col items-center w-full space-y-4">
+          <AiFillCar className="my-4 text-9xl text-primary" />
+          <Title order={1} className="text-ferra">
+            ShareRide
+          </Title>
+          <Text className="font-semibold text-zinc-500">
+            Share your ride with others
+          </Text>
+        </div>
+        <div className="flex flex-col items-center w-full space-y-4">
+          <Button
+            size="lg"
+            className="w-full bg-primary"
+            onClick={() => router.push("/signout")}
+          >
+            Sign Up
+          </Button>
+          <Button
+            size="lg"
+            variant="outline"
+            className="w-full text-primary border-primary"
+            rightIcon={<FaArrowRight className="text-sm" />}
+            onClick={() => router.push("/login")}
+          >
+            Login
+          </Button>
+        </div>
       </div>
     </Layout>
   );
